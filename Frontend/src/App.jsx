cb2834a57ab269cccfab97f9fcc3a892a0518147
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-03-31 21:52:58 +0530

    Rendered the component with a entity

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import HomePage from './components/HomePage'
-import Page from './components/Page'
 function App() {
 
   return (
     <>
       <Navbar/>
-      {/* <HomePage /> */}
-      <Page/>
+      <HomePage />
     </>
   )
 }
--- Frontend/src/components/HomePage.jsx
+++ Frontend/src/components/HomePage.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-export default function HomePage() {
+function Page() {
+  const [data, setData] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const fetchData = () => {
+    fetch("http://localhost:8081/api")
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === data.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000); // Adjust sliding interval as needed
+
+    return () => clearInterval(interval);
+  }, [data]);
+
   return (
-    <> <div className=" flex bg-center justify-center h-[70vh] max-w-full bg-cover bg-no-repeat" style={{backgroundImage: "url('/wallpaperflare.com_wallpaper.jpg')"}}>
-      <p className="text-6xl mt-[10vh] text-white font-serif">Welcome to the crazy and weird world of vehicles</p>
+    <div className="text-center">
+      <p className="text-6xl mt-10 mb-8 text-black font-serif">
+        Welcome to the crazy and weird world of vehicles
+      </p>
+      <div className="overflow-hidden h-[60vh]">
+        <div
+          className="flex transition-transform duration-1000 ease-in-out"
+          style={{
+            transform: `translateX(-${currentIndex * 100}%)`,
+          }}
+        >
+          {data.map((wierdy) => (
+            <div
+              key={wierdy._id}
+              className="flex-shrink-0 w-full flex flex-col justify-center items-center"
+            >
+              <img
+                className="h-[50vh] object-cover"
+                src={wierdy.ImgURL}
+                alt={wierdy.Name}
+              />
+              <p className="text-2xl text-black mt-[60vh] absolute w-[15vw]">{wierdy.Name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-    </>
-  )
+  );
 }
+
+export default Page;
