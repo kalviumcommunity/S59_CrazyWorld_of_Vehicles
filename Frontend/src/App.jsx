9f158916bb24ee110705aeaa0ae00b40b2165529
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-04-01 15:07:59 +0530

    All Entities shown in a component together

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import HomePage from './components/HomePage'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import Weirdies from "./components/Weirdies";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 function App() {
-
   return (
     <>
-      <Navbar/>
-      <HomePage />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Weirdies" element={<Weirdies />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
